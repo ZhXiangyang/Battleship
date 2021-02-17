0edfe91e6e0c74ed95ac6bb06fe52417736d049a
ZXY <46813193+ZhXiangyang@users.noreply.github.com>
2021-02-17 13:19:59 +0100

    add unity project

---
+++ Battleship_Unity/Assets/Scripts/Tile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    public int posX { get; set; }
+    public int posY { get; set; }
+    public GameObject tile;
+}
