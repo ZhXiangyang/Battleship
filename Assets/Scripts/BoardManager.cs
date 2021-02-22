9be69c1a7ff64c81f68a94b50149ffba57293f5e
@ <arnaud.francoise@astudent.yncrea.fr>
2021-02-22 17:00:12 +0100

    DragandDrop

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -16,15 +16,25 @@ public class BoardManager : MonoBehaviour
        
     void CreateBoard()
     {
+        
         boardOrigin.transform.position = new Vector3(-(size-1.0f) / 2, -(size-1.0f) / 2, 0);
         for (int x = 0; x < size; x++)
         {
             for (int y = 0; y< size; y++)
             {
+                /*
                 GameObject tile = Instantiate(prefabTile);
                 tileTab[x, y] = tile.GetComponent<Tile>();
+                tile.transform.SetParent(GameObject.FindWithTag("Canvas").transform, false);
                 tile.transform.position = new Vector3(x, y, 0) + boardOrigin.transform.position;
+                */
+                GameObject tile = Instantiate(prefabTile, new Vector3(x, y, 0)+ boardOrigin.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                tile.transform.SetParent(GameObject.FindWithTag("Canvas").transform, false);
+                tile.transform.localScale = new Vector3(1, 1, 1);
             }
         }
     }
+
+    
+    
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,10 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    
     public int posX { get; set; }
     public int posY { get; set; }
     public GameObject tile;
+
+    private void Start()
+    {
+        
+        GameManager gameManager = gameObject.GetComponent<GameManager>();
+    }
+
+    /// <summary>
+    /// If player click on tile
+    /// check if there is ship or not  
+    /// </summary>
+    public void OnMouseDown()
+    {
+        
+    }
+    
 }
